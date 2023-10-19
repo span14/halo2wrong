6868545e9bd2beda0bce3ee55d23011935ae408c
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2023-10-19 10:43:06 +0100

    Update utils.rs

--- halo2wrong/src/utils.rs
+++ halo2wrong/src/utils.rs
@@ -110,8 +110,6 @@ pub struct DimensionMeasurement {
     fixed: Mutex<u64>,
 }
 
-impl halo2::circuit::layouter::SyncDeps for DimensionMeasurement {}
-
 impl DimensionMeasurement {
     fn update<C: Into<Any>>(&self, column: C, offset: usize) {
         let mut lock = match column.into() {
